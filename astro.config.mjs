e35f211c2495c62e1978bb62cfb70f6ea916c958
TempooDev <a.bermudez.freelance@gmail.com>
2025-05-01 16:20:17 +0200

    Fix dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,13 +3,17 @@ import mdx from '@astrojs/mdx';
 import react from '@astrojs/react';
 import rehypeMermaid from 'rehype-mermaid';
 import expressiveCode from 'astro-expressive-code';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig({
   integrations: [expressiveCode(), mdx(), react()],
   markdown: {
     rehypePlugins: [
       [rehypeMermaid, {
-        theme: 'neutral',
+        strategy: 'img-svg',
         mermaidConfig: {
           startOnLoad: true,
           securityLevel: 'loose',
