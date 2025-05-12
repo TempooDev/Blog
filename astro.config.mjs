92c51e3bac9b6baef6ff2a702dcf0db15fa98437
TempooDev <103463822+TempooDev@users.noreply.github.com>
2025-05-12 21:28:40 +0200

    Feature/sitemap (#14)
    * New content
    
    * Intento de mejora de nav
    
    * dar importancia a la clase
    
    * Translate
    
    * fix gh action
    
    * change target
    
    * Add sitemap refresh

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,23 +1,35 @@
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
-import react from '@astrojs/react';
-import rehypeMermaid from 'rehype-mermaid';
-import expressiveCode from 'astro-expressive-code';
-import sitemap from '@astrojs/sitemap';
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
+import react from "@astrojs/react";
+import rehypeMermaid from "rehype-mermaid";
+import expressiveCode from "astro-expressive-code";
+import sitemap from "@astrojs/sitemap";
 
 export default defineConfig({
-  site:'https://antoniobermudez.dev/',
-  integrations: [expressiveCode(), mdx(), react(), sitemap()],
+  site: "https://antoniobermudez.dev/",
+  integrations: [
+    expressiveCode(),
+    mdx(),
+    react(),
+    sitemap({
+      changefreq: "weekly",
+      priority: 0.7,
+      lastmod: new Date("2025-05-02"),
+    }),
+  ],
   markdown: {
     rehypePlugins: [
-      [rehypeMermaid, {
-        strategy: 'img-svg',
-        mermaidConfig: {
-          startOnLoad: true,
-          securityLevel: 'loose',
-          fontFamily: 'monospace'
-        }
-      }]
-    ]
-  }
+      [
+        rehypeMermaid,
+        {
+          strategy: "img-svg",
+          mermaidConfig: {
+            startOnLoad: true,
+            securityLevel: "loose",
+            fontFamily: "monospace",
+          },
+        },
+      ],
+    ],
+  },
 });
